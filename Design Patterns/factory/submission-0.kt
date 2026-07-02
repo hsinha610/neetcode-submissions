2a5c55d01ebbef96ef881cad5885e6d94f55da6f
HARSHIT SINHA <47042785+hsinha610@users.noreply.github.com>
2026-07-02 05:39:14 +0530

    Add: factory - submission-0

---
+++ Design Patterns/factory/submission-0.kt
@@ -0,0 +1,37 @@
+interface Vehicle {
+    fun getType(): String
+}
+
+class Car : Vehicle {
+    override fun getType(): String {
+        return "Car"
+    }
+}
+
+class Bike : Vehicle {
+    override fun getType(): String {
+        return "Bike"
+    }
+}
+
+class Truck : Vehicle {
+    override fun getType(): String {
+        return "Truck"
+    }
+}
+
+abstract class VehicleFactory {
+    abstract fun createVehicle(): Vehicle
+}
+
+class CarFactory : VehicleFactory() {
+    override fun createVehicle() = Car()
+}
+
+class BikeFactory : VehicleFactory() {
+    override fun createVehicle() = Bike()
+}
+
+class TruckFactory : VehicleFactory() {
+    override fun createVehicle() = Truck()
+}
